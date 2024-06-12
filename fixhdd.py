c7586314a7d5594ff14da2c96e69fe1c594ee913
Uli Köhler <ukoehler@techoverflow.net>
2024-06-12 21:40:39 +0200

    Fix encoding/decoding process

--- fixhdd.py
+++ fixhdd.py
@@ -129,11 +129,10 @@ def getBadSectors(device):
     "Parse a list of recently read bad sectors from the syslog"
     #TODO this gets ALL bad sectors from ALL devices, not only the selected device
     try:
-        out = str(subprocess.check_output('dmesg | egrep "Buffer I/O error on " | grep %s' % device.split('/')[-1], shell=True))
+        out = subprocess.check_output('dmesg | egrep "Buffer I/O error on " | grep %s' % device.split('/')[-1], shell=True).decode("iso-8859-1")
         for line in out.replace('\\n','\n').replace("'",'').split("\n"):
             line = line.strip()
             if not line: continue
-            line = line.decode("iso-8859-1") # In contrast to UTF-8, this encoding can't fail
             # Line is like [72058.852747] Buffer I/O error on dev sdc, logical block in range 348160 + 0-2(12) , async page read
             # and we want to extract the range
             print(f"Found bad sector in syslog: '{line}'")
